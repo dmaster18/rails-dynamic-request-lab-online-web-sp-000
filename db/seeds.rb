1b2bd243242a4ac0f4c00d3387bd375662954e9e
David Strauch <dmaster18@gmail.com>
2020-04-04 18:44:32 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Student.create(first_name: "Bob", last_name: "Sanchez")
+Student.create(first_name)
+    t.string   "first_name"
+    t.string   "last_name"
